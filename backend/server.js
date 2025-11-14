ea65baf693201cde2577bcfd6f9882eaffd04327
jigneshbhandi09 <jigneshbhandi18@gmail.com>
2025-11-14 12:54:26 +0530

    final

--- backend/server.js
+++ backend/server.js
@@ -163,6 +163,7 @@ app.delete("/cart/:id", async (req, res) => {
 
 /* ---------------------- SERVER ---------------------- */
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`✅ Server running on http://localhost:${PORT}`);
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`✅ Server running on port ${PORT}`);
 });
